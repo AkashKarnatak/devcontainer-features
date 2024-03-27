b026c700b85be1f9156e5873c7d16e40f2040009
AkashKarnatak <akashkarnatak.ak47@gmail.com>
2024-03-27 12:37:30 +0530

    Added library script to ensure nanolayer installation

--- src/nvim/install.sh
+++ src/nvim/install.sh
@@ -3,8 +3,10 @@ set -e
 
 echo "Activating feature 'Neovim'"
 
-curl -sfL https://github.com/devcontainers-contrib/nanolayer/releases/download/v0.4.0/nanolayer-x86_64-unknown-linux-gnu.tgz | tar fxvz - \
-  && ./nanolayer install apt --ppas ppa:neovim-ppa/unstable neovim \
-  && rm ./nanolayer \
+source ./library_scripts.sh
+
+ensure_nanolayer nanolayer_location "v0.4.39"
+
+$nanolayer_location install apt --ppas ppa:neovim-ppa/unstable neovim \
   && mkdir -p $_CONTAINER_USER_HOME/.config/nvim $_CONTAINER_USER_HOME/.local/share/nvim \
   && chown -R $_CONTAINER_USER:$_CONTAINER_USER $_CONTAINER_USER_HOME/.config $_CONTAINER_USER_HOME/.local
