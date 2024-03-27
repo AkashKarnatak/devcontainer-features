5b26a247c535fa0792ba3b65224cf7b02a89a533
AkashKarnatak <akashkarnatak.ak47@gmail.com>
2024-03-27 12:55:14 +0530

    Updated workflow

--- src/dotfiles/install.sh
+++ src/dotfiles/install.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 echo "Copying dotfiles"
 
 [ "$INCLUDEBASHRC" = "no" ] && rm -f ./dotfiles/.bashrc
-cp -r ./dotfiles/. $_CONTAINER_USER_HOME/
+cp -rT ./dotfiles $_CONTAINER_USER_HOME/
 chown -R $_CONTAINER_USER:$_CONTAINER_USER $_CONTAINER_USER_HOME
